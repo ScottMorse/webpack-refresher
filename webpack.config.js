386ee00cbe53c0e6be2af8a3f7fb524fddac0430
Scott Morse <scottmorsedev@gmail.com>
2022-05-20 17:39:13 -0500

    Compile basic bundle from TS

--- webpack.config.js
+++ webpack.config.js
@@ -1,1 +1,13 @@
-import path from "path";
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.ts",
+  output: {
+    filename: "bundle.js",
+    path: path.resolve(__dirname, "build"),
+  },
+  module: {
+    rules: [{ test: /\.tsx?$/, use: "ts-loader", exclude: /node_modules/ }],
+  },
+  resolve: { extensions: [".tsx", ".ts", ".js"] },
+};
