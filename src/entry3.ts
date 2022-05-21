d8467e2ce7c0ee29500de098e5ea4039c71cc7ca
Scott Morse <scottmorsedev@gmail.com>
2022-05-21 13:05:10 -0500

    Entry point config object basics

---
+++ src/entry3.ts
@@ -0,0 +1,3 @@
+console.log("entry3");
+// @ts-ignore
+console.log(Library.LIBRARY_NAME);
--- webpack.config.js
+++ webpack.config.js
@@ -2,11 +2,30 @@ const path = require("path");
 
 module.exports = {
   entry: {
-    index: "./src/index.ts",
-    index2: "./src/index2.ts",
+    entry1: {
+      import: "./src/entry1.ts",
+      publicPath: "customPublicPath/", //?
+    },
+    entry2: {
+      import: "./src/entry2.ts",
+      dependOn: "entry3",
+    },
+    entry3: {
+      import: "./src/entry3.ts",
+      runtime: "entry3Runtime", // ?
+    },
+    library: {
+      import: "./src/libraryEntry.ts",
+      filename: "[name].[contenthash].output-lib.js",
+      library: {
+        name: "Library",
+        // type: "commonjs2",
+        type: "var",
+      },
+    },
   },
   output: {
-    filename: "bundle.[contenthash].js",
+    filename: "[name].[contenthash].js",
     path: path.resolve(__dirname, "build"),
   },
   module: {
